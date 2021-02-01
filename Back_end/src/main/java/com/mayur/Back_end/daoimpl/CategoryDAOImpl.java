c527fafbabb1d771ecbeb69530342f42811aa720
mayur jain <jain877mayur@gmail.com>
2021-02-02 02:26:57 +0530

    configured the hibernate and category entity

--- Back_end/src/main/java/com/mayur/Back_end/daoimpl/CategoryDAOImpl.java
+++ Back_end/src/main/java/com/mayur/Back_end/daoimpl/CategoryDAOImpl.java
@@ -1,62 +1,86 @@
 package com.mayur.Back_end.daoimpl;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.mayur.Back_end.dao.CategoryDAO;
 import com.mayur.Back_end.dto.Category;
 
 @Repository("categoryDAO")
+@Transactional
 public class CategoryDAOImpl implements CategoryDAO {
 
-	private static List<Category> categories = new ArrayList<>();
-
-	static {
-		Category category = new Category();
-
-		// adding first category
-		category.setId(1);
-		category.setName("Television");
-		category.setDescription("This is some description for television");
-		category.setImageURL("CAT_1.png");
-
-		categories.add(category);
-
-		category = new Category();
-
-		// adding 2nd category
-		category.setId(2);
-		category.setName("Mobile");
-		category.setDescription("This is some description for Mobile");
-		category.setImageURL("CAT_2.png");
-
-		categories.add(category);
-
-		category = new Category();
-
-		// adding 3rd category
-		category.setId(3);
-		category.setName("Laptop");
-		category.setDescription("This is some description for Laptop");
-		category.setImageURL("CAT_3.png");
-
-		categories.add(category);
-	}
+	@Autowired
+	private SessionFactory sessionFactory;
 
 	@Override
 	public List<Category> list() {
-		return categories;
+		
+		String selectActiveCategory = "FROM Category WHERE active = :active";
+		
+		Query query = sessionFactory.getCurrentSession().createQuery(selectActiveCategory);
+		
+		query.setParameter("active", true);
+		
+		return query.getResultList();
+	}
+
+	// getting single category based on id
+	@Override
+	public Category get(int id) {
+
+		return sessionFactory.getCurrentSession().get(Category.class, Integer.valueOf(id));
 	}
 
 	@Override
-	public Category get(int id) {
-		for(Category category : categories)
-		{
-			if(category.getId() == id) return category;
+	public boolean add(Category category) {
+
+		try {
+
+			// add the category to the database table
+			sessionFactory.getCurrentSession().persist(category);
+			return true;
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			return false;
 		}
-		return null;
+
+	}
+
+	// upadating a single category
+	@Override
+	public boolean update(Category category) {
+
+		try {
+
+			// add the category to the database table
+			sessionFactory.getCurrentSession().update(category);
+			return true;
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			return false;
+		}
+	}
+
+	@Override
+	public boolean delete(Category category) {
+		
+		category.setActive(false);
+		
+		try {
+
+			// add the category to the database table
+			sessionFactory.getCurrentSession().update(category);
+			return true;
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			return false;
+		}
 	}
 
 }
